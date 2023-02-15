75246eb9817c325b6bc4e31204150ab73daad6e7
jrg4ngst3r <jrpp198@gmail.com>
2023-02-14 23:58:27 -0300

    falta retocar los links || agrego los toggle y la x del input

--- script/toggles.js
+++ script/toggles.js
@@ -1,17 +1,25 @@
 const toggle = document.querySelector("#chocolatte");
 
 const menu = document.querySelector("#menu");
+const input = document.querySelector("#searchVal");
+const dDown = document.querySelector(".search_bar__drop_down");
 
 document.addEventListener("click", (e) => {
-  console.log(e);
   if (e.target.parentElement.id === "chocolatte") {
     if (menu.classList.contains("hidden")) {
       menu.classList.replace("hidden", "block");
     } else {
       menu.classList.replace("block", "hidden");
     }
+  } else if (e.target.id === "searchVal" || input.value !== "") {
+    if (dDown.classList.contains("hidden")) {
+      dDown.classList.replace("hidden", "flex");
+    } else {
+      dDown.classList.replace("flex", "hidden");
+    }
   } else {
     menu.classList.replace("block", "hidden");
+    dDown.classList.replace("flex", "hidden");
   }
 });
 
